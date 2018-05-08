806f400e63f2d52a069c98a9da4cba8bf81ded8f
nolit <kj2806@mail.ru>
2018-05-09 02:27:43 +0300

    Полностью реализовал сбор данных с форм Login и Registration, но надо добавить действия с этими данными

--- app/src/main/java/com/example/nolit/collaborationworldofteamcraft/Login.java
+++ app/src/main/java/com/example/nolit/collaborationworldofteamcraft/Login.java
@@ -2,10 +2,10 @@ package com.example.nolit.collaborationworldofteamcraft;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -15,8 +15,29 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
     }
 
+    //Функция для кнопки ВОЙТИ, менять надо, наверное, на главный экран будет отправлять
     public void login(View view) {
         Intent intent = new Intent(Login.this, NewPost.class);
         startActivity(intent);
+    }
+
+    //Функция для кнопки РЕГИСТРАЦИЯ, менять не надо, готово
+    public void registration (View view) {
+        Intent intent = new Intent(Login.this, Registration.class);
+        startActivity(intent);
+    }
+
+    //Функция для получения логина в виде строки
+    private String getLogin() {
+        EditText nameField = findViewById(R.id.login);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
+    }
+
+    //Функция для получения пароля в виде строки
+    private String getPassword() {
+        EditText nameField = findViewById(R.id.password);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
     }
 }
---
+++ app/src/main/java/com/example/nolit/collaborationworldofteamcraft/Registration.java
@@ -0,0 +1,61 @@
+package com.example.nolit.collaborationworldofteamcraft;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+
+
+    //Функция для получения имени и фамилии в виде строки
+    private String getNameSurname() {
+        EditText nameField = findViewById(R.id.name);
+        Editable nameEditable = nameField.getText();
+
+        EditText nameField1 = findViewById(R.id.surname);
+        Editable nameEditable1 = nameField1.getText();
+
+        return nameEditable.toString()+" "+nameEditable1;
+    }
+
+    //Функция для получения пароля в виде строки
+    private String getLogin() {
+        EditText nameField = findViewById(R.id.login);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
+    }
+
+    //Функция для получения пароля в виде строки
+    private String getPassword() {
+        EditText nameField = findViewById(R.id.password);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
+    }
+
+    //Функция для получения пароля в виде строки
+    private String getPassword2() {
+        EditText nameField = findViewById(R.id.password2);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
+    }
+
+    //Функция для получения пароля в виде строки
+    private String getContacts() {
+        EditText nameField = findViewById(R.id.contacts);
+        Editable nameEditable = nameField.getText();
+        return nameEditable.toString();
+    }
+
+    public void createAccaunt (String name, String login, String Password, String contacts) {
+        //функция добавления в БД нового пользователя
+    }
+}
